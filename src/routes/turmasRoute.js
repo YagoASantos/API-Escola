32eb95eae1bc49c95ae6d01a7254c1759e60182e
YagoASantos <yagoaguiar9@gmail.com>
2023-06-06 17:21:05 -0300

    Criação do projeto

--- src/routes/turmasRoute.js
+++ src/routes/turmasRoute.js
@@ -3,7 +3,7 @@ const TurmaController = require('../controllers/TurmaController');
 
 const router = Router();
 
-router.get('/turmas', TurmaController.pegaTodasAsTurmas);
+router.get('/turmas', TurmaController.pegaTodasAsTurmas)
 router.get('/turmas/:id', TurmaController.pegaUmaTurma);
 router.post('/turmas', TurmaController.criaTurma);
 router.post('/turmas/:id', TurmaController.restauraTurma);
